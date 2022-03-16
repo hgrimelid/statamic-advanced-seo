127bb873e55fb56afd221107cef2ddc4070c25e2
Michael Aerni <hello@michaelaerni.ch>
2022-03-16 15:05:58 -0700

    Reload the iframe whenever an image value changes

--- resources/js/components/SocialImagesPreviewFieldtype.vue
+++ resources/js/components/SocialImagesPreviewFieldtype.vue
@@ -2,7 +2,7 @@
 
     <element-container @resized="resize">
         <div ref="container" class="relative w-full overflow-hidden" :style="{'padding-top': aspectRatio}">
-            <iframe ref="iframe" class="absolute inset-0 w-full h-full" :src="this.meta.url" scrolling="no"></iframe>
+            <iframe ref="iframe" class="absolute inset-0 w-full h-full" :src="this.meta.url" frameborder="0" scrolling="no"></iframe>
         </div>
     </element-container>
 
@@ -17,9 +17,25 @@
             aspectRatio() {
                 return `${(this.meta.height / this.meta.width) * 100}%`;
             },
+
+            socialImagesFields() {
+                return _.filter(this.$store.state.publish.base.values, (value, key) => key.includes('seo_social_images_'));
+            },
+        },
+
+        watch: {
+
+            socialImagesFields() {
+                this.refresh()
+            }
+
         },
 
         methods: {
+            refresh() {
+                this.$refs.iframe.src = this.$refs.iframe.src;
+            },
+
             resize() {
                 this.$refs.iframe.contentDocument.body.style.transformOrigin="top left"
                 this.$refs.iframe.contentDocument.body.style.transform=`scale(${this.getContainerWidth() / this.meta.width})`
